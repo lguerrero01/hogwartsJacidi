2edc2d644dfb354b3dc79ff021c215e1d54c2a91
Alejandro <luisguerrero12387@gmail.com>
2022-03-12 09:50:09 -0400

    adding structure and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentFormComponent } from './components/student-form/student-form.component';
+import { TableHogwartsComponent } from './components/table-hogwarts/table-hogwarts.component';
+import { NewStudentsComponent } from './pages/new-students/new-students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentFormComponent,
+    TableHogwartsComponent,
+    NewStudentsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/table-hogwarts/table-hogwarts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-hogwarts',
+  templateUrl: './table-hogwarts.component.html',
+  styleUrls: ['./table-hogwarts.component.css']
+})
+export class TableHogwartsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
